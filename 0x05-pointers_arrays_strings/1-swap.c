6e56f63d69e03e2b131fa6638bb0625c11394df2
JoJoGDS <george.latiko@gmail.com>
2023-03-27 15:19:14 -0700

    1. Dont swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,8 +4,9 @@
  * @a: integer to swap
  * @b: integer to swap
  */
-void swap_int(int *a, int *b) {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
