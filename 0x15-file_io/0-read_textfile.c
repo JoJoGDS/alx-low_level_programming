f40855831e04601ed940bbd2211c5984f0af51f4
JoJoGDS <george.latiko@gmail.com>
2023-05-08 13:48:40 -0700

    1.under the snow

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,45 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
- * create_file - creates a file
- * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-int create_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd; /* file descriptor */
-	ssize_t bytes_written; /* number of bytes written */
+	char *buf;
+	int fd;
+	ssize_t w;
+	ssize_t r;
 
 	if (filename == NULL) /* check if filename is NULL */
-		return (-1);
+		return (0);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600); /* create or truncate file with rw------- permissions */
+	fd = open(filename, O_RDONLY); /* open file for reading */
+	if (fd == -1) /* check if file opening failed */
+		return (0);
 
-	if (fd == -1) /* check if file creation or opening failed */
-		return (-1);
-
-	if (text_content != NULL) /* check if text_content is not NULL */
+	buf = malloc(sizeof(char) * letters); /* allocate memory for buffer */
+	if (buf == NULL) /* check if memory allocation failed */
 	{
-		bytes_written = write(fd, text_content, strlen(text_content)); /* write text_content to file */
-
-		if (bytes_written == -1) /* check if write failed */
-		{
-			close(fd); /* close file */
-			return (-1);
-		}
+		close(fd); /* close file */
+		return (0);
 	}
 
+	r = read(fd, buf, letters); /* read file into buffer */
+	if (r == -1) /* check if file reading failed */
+	{
+		free(buf); /* free buffer */
+		close(fd); /* close file */
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, buf, r); /* write buffer to standard output */
+
+	free(buf); /* free buffer */
 	close(fd); /* close file */
-	return (1); /* return success */
+	return (w); /* return number of bytes written */
 }
